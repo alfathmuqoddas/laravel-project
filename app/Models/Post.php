de80837ed3852b890bb957a9217538488029be4f
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-09-25 10:21:17 +0700

    updating file

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     protected $table = 'posts';
     public $primaryKey = 'id';
     public $timestamps = true;
+
+    //connect posts to user_id
+    public function user(){
+    	return $this->belongsTo('App\User')
+    }
 }
