d6c5a5d90f4d8c5b0274e5fd45f94d051c73d161
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-12-28 17:38:38 +0700

    update code

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -26,6 +26,6 @@ class DashboardController extends Controller
     {
         $user_id = auth()->user()->id;
         $user = user::find($user_id);
-        return view('dashboard')->with('posts', $user->posts);
+        return view('dashboard')->with('posts', $user -> posts);
     }
 }
